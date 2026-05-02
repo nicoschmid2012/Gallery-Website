af312a7e01f9e754c94981f96c34bb9170844577
nicoschmid2012 <nico.schmid2012@gmail.com>
2026-05-02 18:33:04 +0200

    changing the frontend

--- back_end/webpage/script.js
+++ back_end/webpage/script.js
@@ -2,8 +2,8 @@ function SearchPicture(event) {
   event.preventDefault();
 
   const search = document.getElementById("search-input").value;
-  const img = document.getElementById("pic3");
-  const desc = document.getElementById("desc3");
+  const img = document.getElementById("pic");
+  const desc = document.getElementById("desc");
 
   fetch("/search?s=" + search, {
     method: "GET",
@@ -11,8 +11,9 @@ function SearchPicture(event) {
     .then((response) => response.json())
 
     .then((data) => {
-      img.src = "/picture/" + data.data.picture;
+      img.src = "/picture?p=" + data.data.picture;
       desc.innerHTML = data.data.description;
+      search.value = "";
     })
 
     .catch((error) => {
